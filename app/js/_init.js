711f58f4ce3ed548d5c262c2992aa7aff59622aa
Sam Yerkes <samuelyerkes@gmail.com>
2015-04-12 00:59:14 -0400

    integrated in mobile off canvas menu

--- app/js/_init.js
+++ app/js/_init.js
@@ -1,4 +1,26 @@
 $(document).ready(function(){
+    $('span[class^=currentbranch]').each(function() {
+      var linkToChange = $(this).children('a');
+      $(linkToChange).closest('li').prepend(linkToChange);
+      $(this).parent('li').addClass('active');
+      $(this).remove();
+    });
+    $("#mobile-nav").mmenu({
+        "header": {
+            "title": "Menu",
+            "add": true,
+            "update": true
+        }
+    });
+});
+
+$(window).resize(function() {
+    if ($("#mobile-nav").hasClass("mm-opened") === true ) {
+        $( "#mobile-nav" ).trigger( "close.mm" );
+    }
+});
+
+$(document).ready(function(){
     $(".video").fitVids();
 });
 
